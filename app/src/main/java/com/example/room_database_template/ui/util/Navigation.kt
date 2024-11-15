e111403df692b2a8d9a32658cd0eea6c07c0819d
CJ Sewell <charlessj4107@gmail.com>
2024-11-15 14:22:30 -0600

    feat(wip): Adding a Navigation.kt for new screens.

---
+++ app/src/main/java/com/example/room_database_template/ui/util/Navigation.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DatabaseTemplate(
+  personRepository: PersonRepository
+){
+  val navController = rememberNavController()
+  
+}
